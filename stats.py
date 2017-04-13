bcd060c4a5b6ee897272e092b48a1053f16c21fc
Aryaman Arora <aryaman.arora2020@gmail.com>
2017-04-13 15:22:06 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -5,6 +5,12 @@ from time import sleep
 sense = SenseHat()
 while 1:
     orientation = sense.get_orientation_degrees()
-    print("p: {pitch}\nr: {roll}\ny: {yaw}".format(**orientation))
+    print("Pitch: {pitch}\nRoll: {roll}\nYaw: {yaw}".format(**orientation))
+    print("---")
+    temp = sense.get_temperature()
+    print("Temperature: %s F" % temp*(9/5) + 32)
+    print("---")
+    humidity = sense.get_humidity()
+    print("Humidity: %s %%rH" % humidity)
     sleep(0.1)
     os.system('clear')
