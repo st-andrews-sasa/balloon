2f2497eac348c07a006cf4216baad340f3561abf
Aryaman Arora <aryaman.arora2020@gmail.com>
2017-04-13 15:14:02 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,6 +1,8 @@
 from sense_hat import SenseHat
+import os
 
 sense = SenseHat()
 while 1:
     orientation = sense.get_orientation_degrees()
     print("p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
+    os.system('clear')
